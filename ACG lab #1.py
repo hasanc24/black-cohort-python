14e5c13af71c34840f47127d63776d002d0198aa
Hasan Cheema <hasan.cheema23@gmail.com>
2022-12-20 23:47:51 +0000

    ACG lab #1

---
+++ ACG lab #1.py
@@ -0,0 +1,8 @@
+
+#1st ACG Lap
+#Python implemention goes here 
+
+fahrenheit = float(input("What temperature (in Frhrenheit) would you like converted to Celsius?"))
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit, "F is", round(celsius, 2), "C")
